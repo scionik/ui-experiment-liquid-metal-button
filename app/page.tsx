357a8d1d6dde6f1b621939e5e122db140e0f8c47
Nikola Margetic <nikmargetic.ux@gmail.com>
2026-06-07 22:30:24 +0200

    Use white background

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { LiquidMetalButton } from '@/components/ui/liquid-metal-button';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-6 bg-zinc-950">
+    <main className="flex min-h-screen flex-col items-center justify-center gap-6 bg-white">
       <LiquidMetalButton size="sm">Small</LiquidMetalButton>
       <LiquidMetalButton size="md">Click me</LiquidMetalButton>
       <LiquidMetalButton size="lg">Large button</LiquidMetalButton>
